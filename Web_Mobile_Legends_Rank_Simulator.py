d93af3eb0f4b38671ff822b09fa20b0ffec4cf0b
Naim Sheikh <s.sakib@hotmail.it>
2025-06-21 13:19:19 +0100

    Big Changes!

--- Web_Mobile_Legends_Rank_Simulator.py
+++ Web_Mobile_Legends_Rank_Simulator.py
@@ -1,5 +1,6 @@
 ######## Import Libraries ###################################################################################################################################################################
 
+import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
@@ -28,10 +29,10 @@ def Determine_Result (wr, stars, star_raising, star_raising_cap, max_star_raisin
 def rational_model (x, a, b, c):
     return a / (x - b) + c
 
+###################################################################################################################################################################
+
 def simulation_1 (
         simulation1_number_of_attempts_per_win_rate,
-        histogram_bin,
-        expected_season_end_win_rate,
         starting_rank,
         target_stars,
         star_raising_cap,
@@ -45,7 +46,7 @@ def simulation_1 (
         max_win_rate,
         how_many_different_winrates
 ):
-    print("Simulation 1: Series of Win Rates")
+    st.write("Simulation 1: Series of Win Rates")
     # This simulation assumes you actually are able to play at the level that you are simulating.
 
     win_rate_list = [] # list of win rates, x values of the final plot
@@ -117,6 +118,7 @@ def simulation_1 (
         median_games = [item[0] for item in median_run]
         median_stars = [item[1] for item in median_run]
 
+        simulation_1_Player_Paths = plt.figure()
         plt.plot(median_games, median_stars, label=f'{winrate:.1f}% win rate played an average of {max(median_games)} games')
 
     if show_player_Graph == True:
@@ -126,12 +128,12 @@ def simulation_1 (
         plt.xlabel("Number of games")
         plt.ylabel("Number of stars")
         if (show_player_Legend == True): plt.legend()
-        plt.show()    
+        st.pyplot(simulation_1_Player_Paths) 
 
     if(len(games_to_target) != 0):
 
-        ################### Chat GPT Generated ##################################################################################################################################################
-
+        # Chat GPT Generated 
+        simulation_1_WinRate_Distribution = plt.figure()
         plt.scatter(win_rate_list, games_to_target, label='Data per Win Rate')
         plt.scatter(actual_win_rate_list, games_to_target, label='Actual data per Adjusted Win Rate')
 
@@ -152,27 +154,13 @@ def simulation_1 (
         plt.ylabel("Number of games")
         plt.grid(True)
         plt.legend()
-        plt.show()
-
-        ################### Histogram for expected win rate ###########################################################################################################################
-
-        filtered_pairs = [(xv, yv) for xv, yv in zip(win_rate_list, games_to_target) if xv == expected_season_end_win_rate]
-
-        if filtered_pairs:
-            filtered_x, filtered_y = zip(*filtered_pairs)
-            filtered_x = list(filtered_x)
-            filtered_y = list(filtered_y)
-            plt.hist(list(filtered_y), bins= 1 if len(filtered_y) < histogram_bin else histogram_bin)
-            plt.xlabel(f'Games to {target_stars} stars')
-            plt.ylabel('Frequency')
-            plt.title(f"You need {rational_model(expected_season_end_win_rate, *params)} games to reach {target_stars} stars with {expected_season_end_win_rate}% win rate starting from {starting_rank} stars")
-            plt.show()
-        else:
-            print(f"You need and average of {rational_model(expected_season_end_win_rate, *params)} games to reach {target_stars} stars with {expected_season_end_win_rate}% win rate, starting from Epic I 1 Star")
+        st.pyplot(simulation_1_WinRate_Distribution)
 
     else:
-        print(f"Apologies, but for the chosen range between {min_win_rate}% and {max_win_rate}%, none of the attempts have managed to reach {target_stars} stars within {max_games_to_simulate} games")
-    
+        st.write(f"Apologies, but for the chosen range between {min_win_rate}% and {max_win_rate}%, none of the attempts have managed to reach {target_stars} stars within {max_games_to_simulate} games")
+
+###################################################################################################################################################################
+
 def simulation_2 (
         simulation2_number_of_attempts,
         expected_season_end_win_rate,
@@ -184,16 +172,17 @@ def simulation_2 (
         star_protection_cap,
         show_player_Graph,
         max_games_to_simulate,
-        min_win_rate,
-        max_win_rate,
+        histogram_bin
 ):
 
-    print("Simulation 2: Single Win Rate Analysis")
+    st.write("Simulation 2: Single Win Rate Analysis")
 
     # This simulation assumes you actually are able to play at the level that you are simulating.
 
     actual_win_rate_list = []
     games_to_target = [] # list of games to target stars, y value of the final plot
+
+    simulation_2_figure, axes = plt.subplots(2, 2, figsize=(15, 8))
 
     for iterator in range (0, simulation2_number_of_attempts):
 
@@ -246,21 +235,20 @@ def simulation_2 (
 
         x = [item[0] for item in plotting_values]
         y = [item[1] for item in plotting_values]
-        plt.plot(x, y,color='black')
+        axes[0,0].plot(x, y,color='black')
 
     if show_player_Graph == True:
-        plt.grid(True)
-        plt.title(f"Number of Games Required to Return to {target_stars} stars")
-        plt.axhline(target_stars, color='r', label=f'{target_stars} stars threshold')
-        plt.xlabel("Number of games")
-        plt.ylabel("Number of stars")
-        plt.show()    
+        axes[0,0].grid(True)
+        axes[0,0].set_title(f"Number of Games Required to Return to {target_stars} stars")
+        axes[0,0].axhline(target_stars, color='r', label=f'{target_stars} stars threshold')
+        axes[0,0].set_xlabel("Number of games")
+        axes[0,0].set_ylabel("Number of stars")   
 
     if(len(games_to_target) != 0):
 
-        ################### Chat GPT Generated ##################################################################################################################################################
-
-        plt.scatter(actual_win_rate_list, games_to_target, label='Actual data per Adjusted Win Rate')
+        # Chat GPT Generated 
+        simulation_2_Win_Rate_Distribution = plt.figure()
+        axes[0,1].scatter(actual_win_rate_list, games_to_target, label='Actual data per Adjusted Win Rate')
 
         # Your data
         x_data = np.array(actual_win_rate_list)
@@ -272,96 +260,30 @@ def simulation_2 (
         # Generate smooth curve
         x_fit = np.linspace(min(x_data), max(x_data), 500)
         y_fit = rational_model(x_fit, *params)
-        plt.plot(x_fit, y_fit, color='green', label=f'Best Rational Fit: {params[0]}/(x-{params[1]})+{params[2]}')
+        axes[0,1].plot(x_fit, y_fit, color='green', label=f'Best Rational Fit: {params[0]}/(x-{params[1]})+{params[2]}')
 
-        plt.title(f"Win Rate vs Number of Games Needed for {target_stars} stars")
-        plt.xlabel("Win Rate (%)")
-        plt.ylabel("Number of games")
-        plt.grid(True)
-        plt.legend()
-        plt.show()
+        axes[0,1].set_title(f"Win Rate vs Number of Games Needed for {target_stars} stars")
+        axes[0,1].set_xlabel("Win Rate (%)")
+        axes[0,1].set_ylabel("Number of games")
+        axes[0,1].grid(True)
+        axes[0,1].legend()
 
-        ################### Histogram for expected win rate ###########################################################################################################################
+        # Histogram for expected win rate 
 
-        plt.hist(games_to_target, bins= 1 if len(games_to_target) < histogram_bin else histogram_bin)
-        plt.xlabel(f'Games to {target_stars} stars')
-        plt.ylabel('Frequency')
-        plt.title(f"You need {rational_model(expected_season_end_win_rate, *params)} games to reach {target_stars} stars with {expected_season_end_win_rate}% win rate starting from {starting_rank} stars")
-        plt.show()
+        simulation_2_Games_Histogram, ax = plt.subplots()
+        axes[1,0].hist(games_to_target, bins= 1 if len(games_to_target) < histogram_bin else histogram_bin)
+        axes[1,0].set_xlabel(f'Games to {target_stars} stars')
+        axes[1,0].set_ylabel('Frequency')
+        axes[1,0].set_title(f"You need {rational_model(expected_season_end_win_rate, *params)} games to reach {target_stars} stars with {expected_season_end_win_rate}% win rate starting from {starting_rank} stars")
 
-        plt.hist(actual_win_rate_list, bins= 1 if len(actual_win_rate_list) < histogram_bin else histogram_bin)
-        plt.xlabel(f'Win Rate after reaching {target_stars} stars / %')
-        plt.ylabel('Frequency')
-        plt.title(f"This is the distribution of win rate after you reach {target_stars} stars playing if you're a {expected_season_end_win_rate}% wr player")
-        plt.show()
+        axes[1,1].hist(actual_win_rate_list, bins= 1 if len(actual_win_rate_list) < histogram_bin else histogram_bin)
+        axes[1,1].set_xlabel(f'Win Rate after reaching {target_stars} stars / %')
+        axes[1,1].set_ylabel('Frequency')
+        axes[1,1].set_title(f"This is the distribution of win rate after you reach {target_stars} stars playing if you're a {expected_season_end_win_rate}% wr player")
+        
+        st.pyplot(simulation_2_figure)
 
     else:
-        print(f"Apologies, but for the chosen range between {min_win_rate}% and {max_win_rate}%, none of the attempts have managed to reach {target_stars} stars within {max_games_to_simulate} games")
+        st.write(f"Apologies, but for the chosen {expected_season_end_win_rate}% expected win rate, none of the attempts have managed to reach {target_stars} stars within {max_games_to_simulate} games")
 
-############# Control Panel ##########################################################################################################################################################
-
-# Global Variables
-simulation_path = 2
-expected_season_end_win_rate = 55 # what win rate % to figure out how many games to reach the ceiling
-starting_rank = -29
-target_stars = 100 # 0 stars means zero star in mythic, or 5 start in legend 1
-
-# Simulation 1 Variables
-simulation1_number_of_attempts_per_win_rate = 1000 # how many times does a player with a certain win rate need to play?
-histogram_bin = 50 # Determines how many paitns in each bar bars of the final esimate histogram you want, only shows if expected_season_end_win_rate is in the simulation range
-
-# Simulation 2 Variables
-simulation2_number_of_attempts = 100
-
-# Game Variables
-star_raising_cap = 1000 # 1000 gets you extra star
-max_star_raising_per_game = 200
-max_star_protection_per_game = 200
-star_protection_cap = 1000 # 1000 saves you from losing star
-
-# Player Graph Settings
-show_player_Legend = True
-show_player_Graph = True
-max_games_to_simulate = 10000
-min_win_rate = 90
-max_win_rate = 100
-how_many_different_winrates = 50
-
-################### Simulations ######################################################################################################################################################
-
-match(simulation_path):
-
-    case 1: 
-        simulation_1 (
-            simulation1_number_of_attempts_per_win_rate,
-            histogram_bin,
-            expected_season_end_win_rate,
-            starting_rank,
-            target_stars,
-            star_raising_cap,
-            max_star_raising_per_game,
-            max_star_protection_per_game,
-            star_protection_cap,
-            show_player_Legend,
-            show_player_Graph,
-            max_games_to_simulate,
-            min_win_rate,
-            max_win_rate,
-            how_many_different_winrates
-        )
-        
-    case 2: 
-        simulation_2 (
-            simulation2_number_of_attempts,
-            expected_season_end_win_rate,
-            starting_rank,
-            target_stars,
-            star_raising_cap,
-            max_star_raising_per_game,
-            max_star_protection_per_game,
-            star_protection_cap,
-            show_player_Graph,
-            max_games_to_simulate,
-            min_win_rate,
-            max_win_rate,
-        )
+###################################################################################################################################################################
--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import streamlit as st
 from Web_Mobile_Legends_Rank_Simulator import simulation_1, simulation_2
 
 def main():
-    st.title("Star Progression Simulator")
 
-    st.markdown("Adjust the parameters below, then click **Run Simulation**.")
+    st.title("Mobile Legends Ranked Simulator")
+
+    st.markdown("---")
+
+    st.subheader("Adjust the parameters below, then click Run Simulation.")
 
     # --- Simulation Path ---
     simulation_path = st.selectbox(
@@ -13,69 +16,105 @@ def main():
         format_func=lambda x: f"Simulation {x}"
     )
 
-    # --- Main Parameters ---
-    expected_season_end_win_rate = st.slider("Expected Win Rate (%)", 0, 100, 50)
-    starting_rank = st.number_input("Starting Stars", value=-29)
-    target_stars = st.number_input("Target Stars", value=100)
+    col1, col2= st.columns(2)
+    
+    with col1:
+        with st.container():
+            starting_rank = st.number_input("Starting Stars", value=-29)
+    with col2:
+        with st.container():
+            target_stars = st.number_input("Target Stars", value=100)
 
-    # --- Simulation Settings ---
-    simulation1_number_of_attempts_per_win_rate = st.number_input("Simulation 1: Attempts per Win Rate", value=1000)
-    simulation2_number_of_attempts = st.number_input("Simulation 2: Attempts", value=100)
-    histogram_bin = st.number_input("Histogram Bin Count", value=50)
+    expected_season_end_win_rate = st.slider(
+        "Expected Season End Win Rate (%)",
+        min_value=0.0,
+        max_value=100.0,
+        value=50.0,
+        step=0.01,
+        format="%.1f"
+    )    
 
-    # --- Game Mechanics ---
-    star_raising_cap = st.number_input("Star Raising Cap", value=1000)
-    max_star_raising_per_game = st.number_input("Max Star Raising per Game", value=200)
-    max_star_protection_per_game = st.number_input("Max Star Protection per Game", value=200)
-    star_protection_cap = st.number_input("Star Protection Cap", value=1000)
+    st.markdown("---")
 
-    # --- Graph Settings ---
-    show_player_Legend = st.checkbox("Show Legend on Graph", value=True)
-    show_player_Graph = st.checkbox("Show Player Graph", value=True)
-    max_games_to_simulate = st.number_input("Max Games to Simulate", value=10000)
-    min_win_rate = st.slider("Minimum Simulated Win Rate (%)", 0, 100, 90)
-    max_win_rate = st.slider("Maximum Simulated Win Rate (%)", 0, 100, 100)
-    how_many_different_winrates = st.number_input("Different Win Rates to Simulate", value=50)
+    match simulation_path:
 
-    # --- Run Simulation ---
+        case 1:
+
+            win_rate_range = st.slider(
+                "Simulated Win Rate Range (%)",
+                0, 100, (50, 80)
+            )
+
+            min_win_rate, max_win_rate = win_rate_range
+
+            col1, col2 = st.columns(2)
+            with col1:
+                simulation1_number_of_attempts_per_win_rate = st.number_input("Number of Attempts per Win Rate", value=1000)
+            with col2:
+                how_many_different_winrates = st.number_input("Number of Win Rate to Simulate", value=50)
+
+        case 2:
+            simulation2_number_of_attempts = st.number_input("Number of Runs", value=100)
+
+    st.markdown("---")
+
+    # Sidebar for Additional Settings
+    with st.sidebar:
+
+        st.header("Additional Simulation Controls")
+
+        show_player_Legend = st.checkbox("Show Legend on Graph", value=True)
+        show_player_Graph = st.checkbox("Show Player Graph", value=True)
+        max_games_to_simulate = st.number_input("Max Games to Simulate per Run", value=2500)
+        histogram_bin = st.number_input("Histogram Bin Count", value=50)
+        
+        with st.expander("Advanced Game Settings", expanded = False):
+            star_raising_cap = st.number_input("Star Raising Cap", value=1000)
+            max_star_raising_per_game = st.number_input("Max Star Raising per Game", value=200)
+            max_star_protection_per_game = st.number_input("Max Star Protection per Game", value=200)
+            star_protection_cap = st.number_input("Star Protection Cap", value=1000)
+
     if st.button("Run Simulation"):
-        if simulation_path == 1:
-            simulation_1(
-                simulation1_number_of_attempts_per_win_rate,
-                histogram_bin,
-                simulation_path,
-                expected_season_end_win_rate,
-                starting_rank,
-                target_stars,
-                star_raising_cap,
-                max_star_raising_per_game,
-                max_star_protection_per_game,
-                star_protection_cap,
-                show_player_Legend,
-                show_player_Graph,
-                max_games_to_simulate,
-                min_win_rate,
-                max_win_rate,
-                how_many_different_winrates
-            )
-        elif simulation_path == 2:
-            simulation_2(
-                simulation2_number_of_attempts,
-                simulation_path,
-                expected_season_end_win_rate,
-                starting_rank,
-                target_stars,
-                star_raising_cap,
-                max_star_raising_per_game,
-                max_star_protection_per_game,
-                star_protection_cap,
-                show_player_Legend,
-                show_player_Graph,
-                max_games_to_simulate,
-                min_win_rate,
-                max_win_rate,
-                how_many_different_winrates
-            )
+
+        match simulation_path:
+
+            case 1:
+
+                simulation_1(
+                    simulation1_number_of_attempts_per_win_rate,
+                    starting_rank,
+                    target_stars,
+                    star_raising_cap,
+                    max_star_raising_per_game,
+                    max_star_protection_per_game,
+                    star_protection_cap,
+                    show_player_Legend,
+                    show_player_Graph,
+                    max_games_to_simulate,
+                    min_win_rate,
+                    max_win_rate,
+                    how_many_different_winrates
+                )
+
+            case 2:
+
+                simulation_2(
+                    simulation2_number_of_attempts,
+                    expected_season_end_win_rate,
+                    starting_rank,
+                    target_stars,
+                    star_raising_cap,
+                    max_star_raising_per_game,
+                    max_star_protection_per_game,
+                    star_protection_cap,
+                    show_player_Graph,
+                    max_games_to_simulate,
+                    histogram_bin
+                )
+
+            case _:
+
+                st.write("Unknown simulation type selected.")
 
 if __name__ == "__main__":
     main()
